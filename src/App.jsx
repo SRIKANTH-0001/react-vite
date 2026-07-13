2b9608d6670a7df620f0b8b83ea4fa2b7786cf97
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-07-13 15:38:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, {Component} from "react"
+
+//!1) Class based Component (CBC)
+
+//?In CBC, "render" is mandatory
+
+class App extends Component{
+
+    render(){
+        console.log(this);
+        return <h2>CBC</h2>
+    }
+}
+export default App
+
+// Child(LHS) inherited Parent (RHS)
+
+//i) Function based Component (FBC)
+
+// const App=()=>{
+//     return(
+//         <div>
+//             <h1>Say my Name:Srikanth K</h1>
+//         </div>
+//     )
+// }
+
+// export default App
+
+
