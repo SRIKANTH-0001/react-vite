101013ca4296fa0f2e4474447be3a318a17d2701
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-07-14 15:18:38 +0530

    How to implement css in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,20 @@
 import React, {Component} from "react"
 import Child from "./Child"
 
-//!1) Class based Component (CBC)
-
-//?In CBC, "render" is mandatory
-
-    // class App extends Component{
-
-    //     render(){
-    //         console.log(this);
-    //         return <h2>CBC</h2>
-    //     }
-    // }
-    // export default App
-
-// Child(LHS) inherited Parent (RHS)
-
-//i) Function based Component (FBC)
-
-// const App=()=>{
-//     return(
-//         <div>
-//             <h1>Say my Name:Srikanth K</h1>
-//         </div>
-//     )
-// }
-
-// export default App
-
-//How to use one component inside another component?
-
 const App=()=>{
 
-    const name="SRIKANTH K";
-    const age=21;
+    const design={
+            color:"red",
+            backgroundColor:"pink",
+            width:"30%",
+            height:"30%"
+        }
 
     return(
+        
         <>
-        <h1>I'm {name}</h1>
-        <h3>My age is {21}</h3>
-
-        <br /><p>Closing tag is needed in reactjs</p> 
-        <p>from qspiders reactjs course</p>
-
-        <label htmlFor="name">Name: </label>
-        <input id="name" type="text" />
+            <h1 style={{color:"blue",backgroundColor:"yellow"}}>I'm Srikanth</h1>
+            <button style={design}>Click Me!</button>
         </>
      
     )
