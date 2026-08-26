2aef538e37517c517d8eff729eb8b68fc1860d11
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-08-26 13:20:43 +0530

    How to handle and Perform events in React

--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,30 @@ import React, {Component} from "react"
 import Head1 from "./Child"
 import Header from "./modulecss/header"
 
-function Son(props){
-    return(
-        <div style={{backgroundColor:"lightblue"}}>
-            <h1>I'm the Son Component</h1>
-            <h2>{props.children}</h2>
-        </div>
-    )
-}
 
-function Daughter(props){
-    return(
-        <div style={{backgroundColor:"pink"}}>
-            <h1>I'm the Daughter Component</h1>
-            <h2>{props.children}</h2>
-        </div>
-    )
-}
+const App=()=>{
 
-function Parent(props){
+    const shoot=(a,b)=>{
+        alert(`You have used the gun Named ${a}. The shot you did was amzing!...`);
+        confirm(b.type)
+    }
+    const mouseOver=(event)=>{
+        alert(`Why did you put the cursor over Me!😡`);
+        confirm(event.type)
+    }
+
+
     return(
         <>
-            <h1>I'm the Parent of those Son and Daughter Component!...</h1>
-            <br />
-            <br />
-            <Son>
-                <h3>I was written in the parent Component,But it seems like it's in <b>Son</b> Component.</h3>
-                <br />
-            </Son>
-            <br />
-
-            <Daughter>
-                <h3>I was also written in the parent Component,But it seems like it's in <b>Daughter</b> Component.</h3>
-                <br />
-            </Daughter>
-            <br />
-
-            <ol style={{fontSize:"24px"}}>
-                <h1>The available brief details!...</h1>
-                <li>{props.brand}</li>
-                <li>{props.size}</li>
-                <li>{props.color}</li>
-            </ol>
+        <h1 onCopy={()=>alert('You copied Me!')}>Welcome to the Zooting game!</h1>
+        <button onClick={(b)=>shoot("Ak74",b)}>Hit me to shoot!</button>
+        <br />
+        <br />
+        <br />
+        <br /><br /><br />
+        <h1 style={{backgroundColor:"pink",position:"absolute"}}  onMouseOver={(event)=>mouseOver(event)}>Put the Arrow over Me!</h1>
         </>
     )
 }
 
-export default Parent;
-
-
+export default App;
