133b088572dd6c94315d72bf24ce913073026e61
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-08-26 07:45:45 +0530

    Props.children concepts

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,52 @@ import React, {Component} from "react"
 import Head1 from "./Child"
 import Header from "./modulecss/header"
 
-const App=(args)=>{
-
-    console.log(args);
-
-   const address={state:"TamilNadu",city:"Chennai",place:"Sholinganallur",street:"TNHB"};
-
- 
-
+function Son(props){
     return(
-        <>
-            <Head1 pAddress={address}/>
-            <br />
-            <ul>
-                <li>I'm having the Briefs and the brand is {args.brand}</li>
-                <li>The size of it is {args.size} and the it's color is {args.color}</li>
-            </ul>
-
-        </>
-     
+        <div style={{backgroundColor:"lightblue"}}>
+            <h1>I'm the Son Component</h1>
+            <h2>{props.children}</h2>
+        </div>
     )
 }
 
-export default App
+function Daughter(props){
+    return(
+        <div style={{backgroundColor:"pink"}}>
+            <h1>I'm the Daughter Component</h1>
+            <h2>{props.children}</h2>
+        </div>
+    )
+}
+
+function Parent(props){
+    return(
+        <>
+            <h1>I'm the Parent of those Son and Daughter Component!...</h1>
+            <br />
+            <br />
+            <Son>
+                <h3>I was written in the parent Component,But it seems like it's in <b>Son</b> Component.</h3>
+                <br />
+            </Son>
+            <br />
+
+            <Daughter>
+                <h3>I was also written in the parent Component,But it seems like it's in <b>Daughter</b> Component.</h3>
+                <br />
+            </Daughter>
+            <br />
+
+            <ol style={{fontSize:"24px"}}>
+                <h1>The available brief details!...</h1>
+                <li>{props.brand}</li>
+                <li>{props.size}</li>
+                <li>{props.color}</li>
+            </ol>
+        </>
+    )
+}
+
+export default Parent;
 
 
