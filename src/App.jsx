5367e8407dc2224595e4c89fe1c9d3a5d5c104bd
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-08-28 20:16:32 +0530

    Handling the Dropdown creation and select tags in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import { useState } from "react";
+import { useState } from "react"
 
-function MyForm(){
+function Dropdown(){
 
-    const [myText,setMyText]=useState("");
+    const [val,setVal]=useState("");
 
     const handleChange=(e)=>{
-        setMyText(e.target.value);
+        setVal(e.target.value);
     }
 
-    const handleClick=()=>{
-        alert(`Here is the Given Story!..\n\n ${myText}`)
-    }
 
     return(
         <>
-           <h1>Tell me One Random Story or any Interesting Story About you!...😊</h1>
-           <br /><br />
-
-            <label >Write here : </label><br />
-           <textarea name="textarea" value={myText} onChange={handleChange}></textarea>
-
+            <label >Choose you favourite Film in Marvel</label>
             <br />
-            <button onClick={handleClick}>Done</button>
+            <br />
+            <select onChange={handleChange}>
+                <option value="Iron Man">Iron Man</option>
+                <option value="Spider Man">Spider Man</option>
+                <option value="Doctor Strange">Doctor Strange</option>
+                <option value="None" selected>Others</option>
+            </select>
+
+            <br /><br />
+
+            <button onClick={()=>alert(`Oh!You have selected your fav Film as ${val}`)}>Done!</button>
         </>
     )
 }
 
-export default MyForm;
+export default Dropdown
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./style.css"
-import MyForm from './App';
+import Dropdown from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<MyForm/>);
+ReactDOM.createRoot(document.getElementById('root')).render(<Dropdown/>);
