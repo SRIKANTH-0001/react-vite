9b778419a42c1620d89b46efb6e8e63836367a08
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-08-29 06:21:05 +0530

    Handling the Multiple inputs in ReactJs

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,49 @@
 import { useState } from "react"
 
-function Dropdown(){
+function MultiInput(){
 
-    const [val,setVal]=useState("");
+    const [formData,setFormData]=useState({});
 
     const handleChange=(e)=>{
-        setVal(e.target.value);
+        const name=e.target.name;
+        const value=e.target.value;
+        setFormData((prev)=>({...prev,[name]:value}))
     }
-
 
     return(
         <>
-            <label >Choose you favourite Film in Marvel</label>
+            <h1 style={{textAlign:"center"}}>Welcome to Our bank!..</h1>
             <br />
-            <br />
-            <select onChange={handleChange}>
-                <option value="Iron Man">Iron Man</option>
-                <option value="Spider Man">Spider Man</option>
-                <option value="Doctor Strange">Doctor Strange</option>
-                <option value="None" selected>Others</option>
-            </select>
-
+            <h2 style={{textAlign:"center"}}>Here you can perform Amount Transfer....</h2>
             <br /><br />
 
-            <button onClick={()=>alert(`Oh!You have selected your fav Film as ${val}`)}>Done!</button>
+            <div style={{backgroundColor:"lightsteelblue",marginLeft:"38%",height:"250px",width:"380px"}}>
+                <form style={{padding:"15px",fontSize:"16px"}}>
+                    <h1>Amount Transfer Page</h1>
+                    <br />
+
+                    <label >Enter From Account No. :- &ensp;</label>
+                    <input type="number" name="fanNumber" value={formData.fanNumber} onChange={handleChange} placeholder="FAN Number" required/>
+                    <br /> <br />
+
+                    <label >Enter To Account No. :- &ensp;&emsp;</label>
+                    <input type="number" name="tanNumber" value={formData.tanNumber} onChange={handleChange} placeholder="TAN Number" required/>
+                    <br /> <br />
+
+                    <label >Enter the Amount :- &emsp;&emsp;&emsp;&nbsp;</label>
+                    <input type="number" name="amount" value={formData.amount} onChange={handleChange} placeholder="Amount" required/>
+                    <br /><br />
+
+                    <div>
+                        &emsp;&emsp;&emsp;&emsp;<button style={{ backgroundColor: '#077809', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '4px', cursor: 'pointer', fontWeight: 'bold' }} onClick={()=>alert(`Your Entered Amount ₹${formData.amount} has transferred\n to the Account Number ${formData.tanNumber}!...`)}>Transfer</button>&emsp;&emsp;&emsp;&emsp;
+                        <button style={{ backgroundColor: '#d91a1a', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '4px', cursor: 'pointer', fontWeight: 'bold' }} onClick={()=>confirm('Are you sure that you gonna cancel the Amount transfering process?')?alert("Cancelled"):alert("Not Cancelled")}>Cancel</button>
+                    </div>
+
+                </form>
+            </div>
+
         </>
     )
 }
 
-export default Dropdown
+export default MultiInput
