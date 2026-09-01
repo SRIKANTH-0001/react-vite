0e363f85e5fb858d8ba6c6fc34b6434562a8ad31
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-09-01 16:18:18 +0530

    Giving the styling (css) as a Inline css in ReactJs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import FlowerNames from "./Child";
-import { lazy, Suspense } from "react";
+import { useState } from "react";
 
 function App(){
-    const Flower=lazy(()=>import('./Child.jsx'))
+
+    const [doctor,setDoctor]=useState('');
+
+    const handleChange=(e)=>{
+        setDoctor(e.target.value);
+    }
+
+    const doctorNames=["Stephen","Gowtham","Mukesh","Mubarak"];
+
     return(
-        <Suspense fallback={<div>Component Fetching is going on in the background!.....</div>}>
-            <Flower/>
-        </Suspense>
+        <>
+            <h1 style={{color:"green"}}>Prescripto</h1>
+            <p>Here,You can find All specialized doctors to book an appointment!</p>
+
+            <ol>
+                {doctorNames.map((dName,index)=>(
+                    <li key={index}>{dName}</li>
+                ))}
+            </ol>
+
+            <label>Enter Correct Doctor No : </label>
+            <input type="number" name="dNumber" value={doctor.dNumber} max={4} maxLength={1} onChange={handleChange}/>
+            <br /><br />
+            <button type="submit" onClick={()=>alert(`${doctor <= 4 && doctor >= 1? `Your Appointment is Booked Successfully!..\nThe Doctor Number which you entered is ${doctor}\n The Doctor Name is :- ${doctorNames[doctor-1]}` : "You have Entered Wrong Doctor Number!\nPlease choose any among given one!"}`)}
+             style={{height:"36px",width:"180px",backgroundColor:"green",color:"white",fontSize:"15px",borderRadius:"10px"}}>Book an Appointement</button>
+        </>
     )
 }
+
 export default App;
