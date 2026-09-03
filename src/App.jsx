4de7b3e8e463df1dcf63c8cd7d3903361e042c19
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-09-03 15:49:14 +0530

    ReactJs router concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,25 @@
-import { useState } from "react";
-import btnStyles from './button.module.css';
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Navbar from "./components/Navbar"
 
 function App(){
-
-    const [doctor,setDoctor]=useState('');
-
-    const handleChange=(e)=>{
-        setDoctor(e.target.value);
-    }
-
-    const doctorNames=["Stephen","Gowtham","Mukesh","Mubarak"];
-
     return(
         <>
-            <h1 id="head">Prescripto App</h1>
-            <br />
-            <p>Here,You can find All specialized doctors to book an appointment!</p>
+        <BrowserRouter>
+            <Navbar />
 
-            <ol>
-                {doctorNames.map((dName,index)=>(
-                    <li key={index}>{dName}</li>
-                ))}
-            </ol>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+            </Routes>
 
-            <label>Enter Correct Doctor No : </label>
-            <input type="number" name="dNumber" value={doctor.dNumber} max={4} maxLength={1} onChange={handleChange}/>
-            <br /><br />
-            <button type="submit" onClick={()=>alert(`${doctor <= 4 && doctor >= 1? `Your Appointment is Booked Successfully!..\nThe Doctor Number which you entered is ${doctor}\n The Doctor Name is :- ${doctorNames[doctor-1]}` : "You have Entered Wrong Doctor Number!\nPlease choose any among given one!"}`)}
-            className="btn">Book an Appointement</button>
+        </BrowserRouter>
 
-            <button className="btn1" style={{marginLeft:"20px"}} onClick={()=>alert('The last Appointment Which You booked is Cancelled!')}>Cancel</button>
         </>
     )
 }
 
-export default App;
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import divStyle from "../Styles.module.css"
+
+function Home(){
+    return(
+        <>
+            <div className={divStyle.primary}>
+                <div className={divStyle.secondary}>
+                    <h2 style={{color:"red"}}>Welcome to SriShop!</h2><br /> <h3 style={{color:"darkgreen"}}>Here,You can get whatever You want to buy..!</h3>
+
+                    <h1 style={{marginTop:"20px",color:"purple"}}>Happy Buying!...</h1>
+                </div>
+
+                
+            </div>
+        </>
+    )
+}
+
+export default Home
