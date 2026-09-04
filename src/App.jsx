d1682f5cdf1da34c392666e99892c1fdb7b12bd6
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-09-04 08:40:08 +0530

    Nested Routes in react

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Navbar from "./components/Navbar"
+import Product, { WatchProduct,MobileProduct } from "./pages/Product"
 
 function App(){
     return(
         <>
         <BrowserRouter>
             <Navbar />
-
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/about" element={<About/>}/>
                 <Route path="/contact" element={<Contact/>}/>
-            </Routes>
+                <Route path="/product" element={<Product/>}/>
+                <Route path="/product/mobiles" element={<MobileProduct/>}/>
+                <Route path="/product/watches" element={<WatchProduct/>}/>    
 
+            </Routes>
         </BrowserRouter>
 
         </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,11 @@ function Navbar(){
             <a href="/" style={{fontSize:"30px",fontWeight:"bold",marginLeft:"20px",textDecoration:"none",color:"black"}}>SriShop🏚️</a>
             <div style={{display:"flex",gap:"3rem",alignItems:"center",fontSize:"25px",fontWeight:"bold"}}>
                 <Link style={{textDecoration:"none",color:"black"}} to="/">Home</Link>
+                <Link style={{textDecoration:"none",color:"black"}} to="/product">Product</Link>
                 <Link style={{textDecoration:"none",color:"black"}} to="/about">About</Link>
                 <Link style={{textDecoration:"none",color:"black"}} to="/contact">Contact</Link>
             </div>
-            <button onClick={()=>alert('You have logged out from Your account successfully!....')} style={{cursor:"pointer",height:"32px",width:"80px",border:"none",backgroundColor:"wheat",color:"black",fontSize:"15px",borderRadius:"5px",marginTop:"10px",marginRight:"10px",fontWeight:"bolder"}}>Logout</button>
+            <button onClick={()=>alert('You have logged out from Your account successfully!....')} style={{cursor:"pointer",height:"32px",width:"80px",border:"none",backgroundColor:"red",color:"black",fontSize:"15px",borderRadius:"5px",marginTop:"10px",marginRight:"10px",fontWeight:"bolder"}}>Logout</button>
         </nav>
     )
 }
