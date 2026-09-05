db5e1c6241d673560e8100555d7abe897e3b5b2a
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-09-05 14:08:59 +0530

    UseState hook in React(create,update)

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
-import { BrowserRouter, Routes,Route } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Navbar from "./components/Navbar"
-import Product, { WatchProduct,MobileProduct } from "./pages/Product"
+import { useState } from "react";
 
 function App(){
+
+    const [state,setState]=useState("red");
+    const [favCarDetails,setFavCarDetails]=useState({
+        name:"ford",
+        price:"₹12 Lakh",
+        color:"blue",
+        type:"EV"
+    });
+
+    const changeName=()=>{
+        setFavCarDetails((prev)=>{
+            return {...prev,name:"Hundai"};
+        })
+    }
+
     return(
         <>
-        <BrowserRouter>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/contact" element={<Contact/>}/>
-                <Route path="/product" element={<Product/>}/>
-                <Route path="/product/mobiles" element={<MobileProduct/>}/>
-                <Route path="/product/watches" element={<WatchProduct/>}/>    
-
-            </Routes>
-        </BrowserRouter>
+        <h1 style={{textAlign:"center",color:"blue"}}>Just Simple examples for UseState Hook</h1>
+        <div style={{height:"300px",width:"300px",marginTop:"50px",marginLeft:"600px",backgroundColor:"lightgray",display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"space-evenly"}}>
+            <h2 style={{textAlign:"center",color:"tomato"}}>{state}</h2>
+            <button style={{cursor:"pointer",fontSize:"30px",height:"40px",width:"100px",backgroundColor:"red",color:"white",borderRadius:"8px"}} onClick={()=>setState("black")}>
+                click
+            </button>
+        </div>
+        <br />
+        <div style={{backgroundColor:"lightgoldenrodyellow",height:"200px",width:"350px",marginLeft:"600px"}}>
+            <h1>Favourite Car details!</h1><br />
+            <ul style={{color:"rgb(62, 2, 6)",fontSize:"25px"}}>
+                <li>Name of Car: {favCarDetails.name}</li>
+                <li>Price of Car: {favCarDetails.price}</li>
+                <li>Color of Car: {favCarDetails.color}</li>
+                <li>Type of Car: {favCarDetails.type}</li>
+            </ul>
+            <br />
+            <button onClick={()=>changeName()} style={{color:"seagreen",height:"30px",marginTop:"20px",backgroundColor:"ButtonShadow",fontSize:"18px",cursor:"pointer"}}>Click to view My second fav Car Name</button>
+        </div>
 
         </>
     )
