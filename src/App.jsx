fd517a0e0c561ffbd0d86e7780c856e69ace0d5e
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-09-06 19:50:03 +0530

    UseRef hook in ReactJs

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
-import { createContext, useState } from "react"
-import Child from "./Child";
-
-export const UserContext=createContext();
+import { useEffect, useRef, useState } from "react";
 
 function App(){
-    const [name,setName]=useState("SRIKANTH K");
-    const fruits=["Apple","Banana","Grapes","Orange"]
+
+    const [inputVal, setInputVal] = useState("");
+
+    const count=useRef(0);
+
+    useEffect(()=>{
+        count.current=count.current+1;
+    })
+
+    const input=useRef();
+
+    const handleClick=()=>{
+        input.current.focus();
+    }
+
 
     return(
         <>
-            <UserContext.Provider value={fruits}>
-                <div>
-                    <h1>I'm executed from App component!...</h1>  
-                    <br />
-                    <h2>My Name is :- {name}</h2>  
-                </div> 
-                <Child/>
-            </UserContext.Provider>
+            <label >Enter any Number : 
+                <input
+                    type="number" 
+                    value={inputVal}
+                    onChange={(e)=>setInputVal(e.target.value)}
+                />
+            </label>
+            <br />
+            <br />
+            <label >Enter Your age : 
+                <input type="number" ref={input}/>
+            </label><br /><br />
+
+            <button onClick={()=>handleClick()}>Focus</button>
+            <h3>State updates: {count.current}</h3>
+            <p>Type in the input to trigger a re-render.</p>
         </>
     )
 }
 
-export default App
+export default App;
