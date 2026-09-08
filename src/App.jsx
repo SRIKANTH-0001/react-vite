4b87101b1c460b11806382b61ecc17e91f47fbf2
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-09-08 15:16:35 +0530

    Creating the custom hook named "useFetch"

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,20 @@
-import { useMemo, useState } from "react";
-
-const expensiveCalculation=(num)=>{
-    console.log("Calculation is Started!...");
-
-    for(let i=0;i<1000000000;i++){
-        num+=1;
-    }
-    return num;
-}
+import { useEffect, useState } from "react";
+import { useFetch } from "./useFetch";
 
 function App(){
 
-    const [count,setCount]=useState(0);
-    const [todo,setToDo]=useState([]);
-
-
-    //With useMemo Hook - Memoising the result of expensiveCalculation function 
-    const calculation=useMemo(()=>expensiveCalculation(count),[count]);
-    
-    //Without useMemo
-
-    // const calculation=expensiveCalculation(count);
-
-    const handleClick=()=>{
-        setToDo((prev)=>{
-            return [...prev,"New Todo is Added!"];
-        })
-    }
-
-    const handleClickInc=()=>{
-        setCount((c)=>c+1);
-    }
+    const [data]=useFetch("https://jsonplaceholder.typicode.com/todos");
 
 
     return(
-        <>
-            <div>
-                {todo.map((item,index)=>{
-                    return <h3 key={index}>{item}</h3>
-                })}
-                <br /><br />
-                <button onClick={()=>handleClick()}>Add ToDo</button>
-            </div>
-            <br /><br />
-            <div>
-                <h2>Count : {count}</h2>
-                <br />
-                <button onClick={()=>handleClickInc()}>Inc Count</button>
-            </div>
-            <div>
-                <h2>Expensive calculation!</h2>
-                <br />
-                <h4>{calculation}</h4>
-            </div>
+    <>
+        <div>
+            {data && data.map((info)=>(
+                <p key={info.id}>{info.title}</p>
+            ))}
+        </div>
 
-        </>
+    </>
     )
 }
-
 export default App;
